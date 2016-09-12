07691ea5ef3160cee426e63e237df8510c4468b1
Nathiyaram <nathiya121212@gmail.com>
2016-09-12 10:22:46 +0530

    Add files via upload

---
+++ ideone_pJDhb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int a[100],b;
+
+int main(void) {
+	// your code goes here
+		//Enter the array size
+	scanf("%d",&b);
+	//Enter your array
+	
+	for(int i=o;i<b;i++)
+	{
+		scanf("%d",a[i]);
+	}
+func();
+}
+
+void func();
+
+{
+int t;
+for(i=0;i<b;i++)
+{
+	if(a[i]>a[i+1])
+	{
+		t=a[i];
+		a[i]=a[i+1];
+		a[i+1]=t;
+	}
+	printf("The numbers which are match with the index are:");
+	for(i=0;i<b;i++)
+	{
+		if(a[i]==i;
+		printf("%d",a[i]);
+	}
+}}
+}
+
+}
